3a26050ccaa898c718d1859cfc7f027b73ead89a
DaGestapo <105201337+DaGestapo@users.noreply.github.com>
2023-03-18 11:12:20 +0300

    add events on menu

--- js/main.js
+++ js/main.js
@@ -1,7 +1,41 @@
 "use strict";
-let headerNavbar = document.querySelectorAll('a[data_link]');
-headerNavbar.forEach(link => {
+let headerNavLinks = document.querySelectorAll('a[data-link]');
+let headerNavBar = document.querySelector('.header__nav');
+let articles = document.querySelectorAll('article');
+let cheker = true;
+articles.forEach(elm => {
+    if (!(elm instanceof (HTMLElement))) {
+        cheker = false;
+    }
+    ;
+});
+if (cheker) {
+    headerNavBar === null || headerNavBar === void 0 ? void 0 : headerNavBar.addEventListener('click', goToElement);
+}
+headerNavLinks.forEach(link => {
     link.addEventListener('click', (event) => {
         event.preventDefault();
     });
 });
+function goToElement(event) {
+    if (!(event.target instanceof (HTMLElement)))
+        return;
+    let target = event.target.closest('a[data-link]');
+    if (!(target instanceof (HTMLElement)))
+        return;
+    console.log(articles[0].getBoundingClientRect().top);
+    switch (target.dataset.link) {
+        case "home":
+            window.scrollTo(0, 0);
+            break;
+        case "about":
+            window.scrollTo(0, articles[0].offsetTop - 70);
+            break;
+        case "photos":
+            window.scrollTo(0, articles[1].offsetTop - 70);
+            break;
+        case "contact":
+            window.scrollTo(0, articles[2].offsetTop - 70);
+            break;
+    }
+}
